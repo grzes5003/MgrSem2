0d8fb168fd83107e39c4859be34e5144175cb567
grzes5003 <grzes.kulinski@gmail.com>
2022-11-01 16:23:20 +0100

    [NLP] Added Lab02 with solutions

--- NLP/util/files.py
+++ NLP/util/files.py
@@ -1,4 +1,5 @@
 import os
+from typing import Iterator, Tuple
 
 
 def read_lines(filepath: str) -> [str]:
@@ -13,3 +14,9 @@ def read_lines_dir(path: str) -> [(str, [str])]:
     # return [(file, read_lines(f'{path}/{file}')) for file in files if file == '1994_195.txt']
     return [(file, read_lines(f'{path}/{file}')) for file in files]
 
+
+def read_files_dir(path: str) -> Iterator[Tuple[str]]:
+    files = os.listdir(path)
+    for filename in files:
+        with open(f'{path}/{filename}', encoding='utf-8') as f:
+            yield filename, f.read()
