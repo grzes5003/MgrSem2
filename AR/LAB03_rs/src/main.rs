8ac8dd8bacce249b9057fbb8e4a744a18979113d
grzes5003 <grzes.kulinski@gmail.com>
2022-11-03 00:40:43 +0100

    [AR] Lab03 initial solution

---
+++ AR/LAB03_rs/src/main.rs
@@ -0,0 +1,111 @@
+use std::env;
+use clap::Parser;
+use mpi::collective::CommunicatorCollectives;
+use mpi::topology::Communicator;
+use ndarray::{Array, array, Array2, Axis};
+use ndarray_npy::write_npy;
+use crate::util::{gen_matrix, get_slices, join_calc, Pair, update_matrix};
+use rand::Rng;
+use rand::rngs::ThreadRng;
+
+mod util;
+
+type Procs = usize;
+type Matrix = Vec<Vec<f32>>;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+pub struct Args {
+    #[arg(long = "it", default_value_t=100)]
+    pub iterations: usize,
+
+    #[arg(short = 'n', default_value_t=100)]
+    pub grid_len: usize,
+
+    #[arg(long = "save", short='s', default_value_t=false)]
+    pub save_results: bool
+}
+
+fn main() {
+    let universe = mpi::initialize().unwrap();
+    let world = universe.world();
+    let root_rank = 0;
+
+    let t_start = mpi::time();
+    world.barrier();
+
+    let p = world.size() as Procs;
+    let rank = world.rank() as Procs;
+
+    // let grid_len = env::args().nth(1)
+    //     .unwrap_or("100".to_string())
+    //     .parse::<Procs>()
+    //     .unwrap();
+    //
+    // let iterations = env::args().nth(2)
+    //     .unwrap_or("100".to_string())
+    //     .parse::<Procs>()
+    //     .unwrap();
+    // let slice_length = (grid_len as f32 / p as f32 + 0.5f32) as Procs + 1;
+    //
+    // let save_result = env::args().nth(3)
+    //     .unwrap_or("0".to_string());
+
+    let args: Args = Args::parse();
+    let (grid_len, iterations, save_result) = (
+        args.grid_len,
+        args.iterations,
+        args.save_results);
+    let slice_length = (grid_len as f32 / p as f32 + 0.5f32) as Procs + 1;
+
+    let pair: Pair = get_slices(&world, rank, p, grid_len);
+    let (begin, end) = (pair.0, pair.1);
+
+    let mut matrix =
+        gen_matrix(slice_length, grid_len + 2, || rand::thread_rng().gen::<f32>());
+
+    println!("{:?}", matrix.get(0));
+
+    for y in 0..(end - begin + 1) {
+        matrix[y][0] = 0f32;
+        matrix[y][grid_len + 1] = 0f32;
+    }
+
+    let mut matrix_tmp = gen_matrix(slice_length, grid_len + 2, || 0f32);
+    let up_slice = vec![0f32; grid_len as usize + 2];
+    let down_slice = vec![0f32; grid_len as usize + 2];
+    let mut result = gen_matrix(slice_length, grid_len + 2, || 0f32);
+
+
+    for _ in 0..iterations {
+        matrix = update_matrix(&world, rank, p, grid_len,
+                               matrix, &mut matrix_tmp,
+                               up_slice.clone(), down_slice.clone(),
+                               Pair(begin, end));
+    }
+
+    join_calc(&world, rank, p, grid_len, matrix, slice_length, &mut result);
+    world.barrier();
+
+    if rank == root_rank {
+        // println!("{:?}", result[0]);
+        let t_end = mpi::time();
+        println!("t={};it={};n={}", t_end - t_start, iterations, grid_len);
+        if save_result {
+            // let mut arr = Array2::zeros((result.len(), result[0].len()));
+            // let v = result.into_iter()
+            //     .map(|row| Array::from_vec(row))
+            //     .collect();
+            let mut arr = Array2::from_shape_vec(
+                (result.len(), result[0].len()),
+                result.into_iter().flatten().collect::<Vec<f32>>()).unwrap().to_owned();
+
+            // let result1 =
+            //     Array2::from_shape_vec((result.len(), result[0].len()), v);
+            // for (i, &mut row) in arr.axis_iter_mut(Axis(0)).enumerate() {
+            //     row = Array::from_vec(result[i].clone())
+            // }
+            write_npy("result.npy", &arr);
+        }
+    }
+}
--- AR/LAB03_rs/src/util.rs
+++ AR/LAB03_rs/src/util.rs
@@ -1,9 +1,12 @@
+use std::io::BufRead;
 use mpi::Rank;
 use mpi::topology::SystemCommunicator;
 use mpi::traits::{Communicator, Destination, Source};
 use mpi::traits::Equivalence;
-use crate::Procs;
+use clap::Parser;
 use rand::Rng;
+
+use crate::{Matrix, Procs};
 
 type Float = f32;
 
@@ -34,14 +37,116 @@ pub fn get_slices(comm: &SystemCommunicator, rank: Procs, p: Procs, grid_len: Pr
     }
 }
 
-pub fn gen_matrix(x: usize, y: usize) -> Vec<Vec<Float>> {
-    let mut rng = rand::thread_rng();
-
+pub fn gen_matrix<F>(x: usize, y: usize, generator: F) -> Vec<Vec<Float>>
+    where F: FnOnce() -> Float + Copy {
     (0..x).into_iter()
-        .map(|_| vec![rng.gen::<Float>(); y])
+        .map(|_| (0..y).map(|_| generator()).collect())
         .collect()
 }
 
-pub fn update_matrix(comm: &SystemCommunicator, rank: Procs, p: Procs, grid_len: Procs) {
+pub fn update_matrix(comm: &SystemCommunicator, rank: Procs, p: Procs, grid_len: Procs,
+                     matrix: Matrix, matrix_tmp: &mut Matrix,
+                     mut up_slice: Vec<Float>, mut down_slice: Vec<Float>, pair: Pair
+) -> Matrix {
+    let (begin, end) = (pair.0, pair.1);
 
+    if rank % 2 == 0 {
+        if rank > 0 {
+            comm.process_at_rank((rank - 1) as Rank)
+                .send(&matrix[0][..]);
+        }
+        if rank < p - 1 {
+            let res = comm.process_at_rank((rank + 1) as Rank)
+                .receive_vec();
+            up_slice = res.0;
+        }
+        if rank < p - 1 {
+            comm.process_at_rank((rank + 1) as Rank)
+                .send(&matrix[end - begin][..]);
+        }
+        if rank > 0 {
+            let res = comm.process_at_rank((rank - 1) as Rank)
+                .receive_vec();
+            down_slice = res.0;
+        }
+    } else {
+        if rank < p - 1 {
+            let res = comm.process_at_rank((rank + 1) as Rank)
+                .receive_vec();
+            up_slice = res.0;
+        }
+        if rank > 0 {
+            comm.process_at_rank((rank - 1) as Rank)
+                .send(&matrix[0][..]);
+        }
+        if rank > 0 {
+            let res = comm.process_at_rank((rank - 1) as Rank)
+                .receive_vec();
+            down_slice = res.0;
+        }
+        if rank < p - 1 {
+            comm.process_at_rank((rank + 1) as Rank)
+                .send(&matrix[end - begin][..]);
+        }
+    }
+
+
+    let (mut new_down, mut new_up) = (0f32, 0f32);
+    for y in begin..(end + 1) {
+        for x in 1..(grid_len + 1) {
+            if y - 1 == 0 {
+                new_down = 0f32;
+            } else if y - 1 >= begin {
+                new_down = matrix[y - 1 - begin][x];
+            } else {
+                new_down = down_slice[x];
+            }
+
+            if y + 1 == grid_len + 1 {
+                new_up = 0f32;
+            } else if y + 1 <= end {
+                new_up = matrix[y + 1 - begin][x]
+            } else {
+                new_up = up_slice[x]
+            }
+            matrix_tmp[y - begin][x] = (matrix[y - begin][x - 1] + matrix[y - begin][x + 1]
+                + new_up + new_down) / 4f32;
+        }
+    }
+    matrix_tmp.to_owned()
+}
+
+pub fn join_calc(comm: &SystemCommunicator, rank: Procs, p: Procs, grid_len: Procs,
+             matrix: Matrix, slice_len: Procs, result: &mut Matrix) {
+    if rank == 0 {
+        let mut idx = 0;
+        for row in matrix {
+            if row[1] == 0f32 {
+                break;
+            }
+            result[idx] = row;
+            idx += 1;
+        }
+
+        for i in 0..(p - 1) {
+            let res = comm.process_at_rank((i + 1) as Rank)
+                .receive_vec::<Float>();
+            let tmp: Matrix = res.0.chunks(slice_len)
+                .map(|v| v.to_vec())
+                .collect();
+
+            for x in tmp {
+                if x[1] == 0f32 {
+                    break;
+                }
+                result[idx] = x;
+                idx += 1;
+            }
+
+        }
+    } else {
+        let flatten_matrix: Vec<Float> = matrix.into_iter().flatten().collect();
+        comm.process_at_rank(0)
+            .send(&flatten_matrix[..])
+    }
 }
