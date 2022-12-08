784818f42d4a0b2576faa7be876e2717aec2e4eb
grzes5003 <grzes.kulinski@gmail.com>
2022-12-08 20:39:29 +0100

    [STUD-216][NLP] Lab07 solutions

--- NLP/lab07.py
+++ NLP/lab07.py
@@ -1,15 +1,21 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
 from datasets import load_dataset, Dataset
 from sklearn.feature_extraction.text import TfidfTransformer, TfidfVectorizer
-from sklearn.metrics import f1_score, precision_score, accuracy_score, recall_score
+from sklearn.metrics import f1_score, precision_score, accuracy_score, recall_score, ConfusionMatrixDisplay
 from sklearn.naive_bayes import GaussianNB
 import fasttext
+import evaluate
+from lime.lime_text import LimeTextExplainer
+from sklearn.metrics import confusion_matrix
 
 from transformers import DataCollatorForTokenClassification, AutoModelForSequenceClassification, TrainingArguments, \
-    Trainer
+    Trainer, AutoTokenizer, DataCollatorWithPadding
 from transformers import pipeline
 
 
-def bayesian_classifier(dataset: Dataset):
+def bayesian_classifier(dataset: Dataset, ver):
     vectorizer = TfidfVectorizer()
     vectorizer.fit(dataset['train']['text'])
     tgidf_train, tgidf_test = vectorizer.transform(dataset['train']['text']).toarray(), \
@@ -19,18 +25,20 @@ def bayesian_classifier(dataset: Dataset):
     gnb1.fit(tgidf_train, dataset['train']['label'])
 
     # evaluate
-    return evaluate_bayesian(gnb1.predict(tgidf_test), dataset['test']['label'], 'GaussianNaiveBayes')
+    return evaluate_bayesian(gnb1.predict(tgidf_test), dataset['test']['label'], 'GaussianNaiveBayes', ver)
 
 
 def evaluate_bayesian(predicted, actual, name: str, ver: int = 1):
     if ver == 1:
         return {"name": name, "accuracy": accuracy_score(predicted, actual),
                 "precision": precision_score(predicted, actual),
-                "recall": recall_score(predicted, actual), "f1": f1_score(predicted, actual)}
+                "recall": recall_score(predicted, actual), "f1": f1_score(predicted, actual),
+                'type': ver}
     return {"name": name, "accuracy": accuracy_score(predicted, actual),
             "precision": precision_score(predicted, actual, average='macro'),
             "recall": recall_score(predicted, actual, average='macro'),
-            "f1": f1_score(predicted, actual, average='macro')}
+            "f1": f1_score(predicted, actual, average='macro'),
+            'type': ver}
 
 
 def fasttext_format(name: str, dataset: Dataset):
@@ -44,39 +52,137 @@ def fasttext_format(name: str, dataset: Dataset):
 def fasttext_classifier(filename: str, path: str = '.'):
     model = fasttext.train_supervised(f'{path}/{filename}_train.txt')
     result = model.test(f'{path}/{filename}_test.txt')
-    return {"name": "FastText", "accuracy": result[1], "precision": None, "recall": None, "f1": None}
+    return {"name": "FastText", "accuracy": result[1], "precision": None, "recall": None, "f1": None,
+            'type': int(filename[-1])}
 
 
-def transformer_classifier(dataset: Dataset, name: str, num_labels: int):
-    _pipeline = pipeline('fill-mask', model='allegro/herbert-base-cased')
-    t_train, t_test = _pipeline.tokenizer(dataset['train']['text']), \
-        _pipeline.tokenizer(dataset['test']['text'])
+def compute_metrics(eval_pred):
+    accuracy = evaluate.load("accuracy")
 
-    data_collator = DataCollatorForTokenClassification(_pipeline.tokenizer)
-    model = AutoModelForSequenceClassification.from_pretrained('allegro/herbert-base-cased', num_labels=num_labels)
+    predictions, labels = eval_pred
+    predictions = np.argmax(predictions, axis=1)
+    return accuracy.compute(predictions=predictions, references=labels)
+
+
+def transformer_classifier(dataset: Dataset, name: str, num_labels: int, id2label: dict, label2id: dict):
+    tokenizer = AutoTokenizer.from_pretrained("allegro/herbert-base-cased")
+    data_collator = DataCollatorWithPadding(tokenizer=tokenizer)
+
+    def preprocess_function(examples):
+        return tokenizer(examples["text"], truncation=True)
+
+    tokenized_data = dataset.map(preprocess_function, batched=True)
+    model = AutoModelForSequenceClassification.from_pretrained('allegro/herbert-base-cased',
+                                                               num_labels=num_labels)
 
     training_args = TrainingArguments(
-        output_dir='results/lab07',
-        learning_rate=0.00002,
+        output_dir=f'results/lab07/var_{num_labels}',
+        learning_rate=2e-5,
         per_device_train_batch_size=16,
         per_device_eval_batch_size=16,
         num_train_epochs=2,
         weight_decay=0.01,
-        evaluation_strategy="epoch"
+        evaluation_strategy="epoch",
+        save_strategy="epoch",
+        load_best_model_at_end=True
     )
 
     trainer = Trainer(
         model=model,
         args=training_args,
-        train_dataset=dataset['train'],
-        eval_dataset=dataset['test'],
-        tokenizer=model.tokenizer,
-        data_collator=data_collator
+        train_dataset=tokenized_data['train'],
+        eval_dataset=tokenized_data['test'],
+        tokenizer=tokenizer,
+        data_collator=data_collator,
+        compute_metrics=compute_metrics
     )
 
     trainer.train()
-    trainer.save_pretrained(name)
+    model.save_pretrained(name)
+
+    evaluate_transformers(model, dataset, tokenizer, compute_metrics)
+
     return model
+
+
+def evaluate_task1(predicted, actual):
+    return {"accuracy": accuracy_score(predicted, actual), "precision": precision_score(predicted, actual),
+            "recall": recall_score(predicted, actual), "f1": f1_score(predicted, actual)}
+
+
+def evaluate_task2(predicted, actual):
+    return {"accuracy": accuracy_score(predicted, actual),
+            "precision": precision_score(predicted, actual, average='macro'),
+            "recall": recall_score(predicted, actual, average='macro'),
+            "f1": f1_score(predicted, actual, average='macro')}
+
+
+def evaluate_transformers(model, dataset, tokenizer, compute_metrics):
+    tokenized_dt = dataset.map(lambda x: tokenizer(x["text"], truncation=True), batched=True)
+    trainer = Trainer(model=model,
+                      eval_dataset=tokenized_dt["test"],
+                      tokenizer=tokenizer,
+                      compute_metrics=compute_metrics)
+
+    return trainer.evaluate()
+
+
+def transform_results():
+    model01 = {'name': 'AutoModelForSequenceClassification',
+               'accuracy': 0.892,
+               'precision': 0.6096308296852918,
+               'recall': 0.8873462428449641,
+               'f1': 0.6490954460386775,
+               'type': 1}
+    model02 = {'name': 'AutoModelForSequenceClassification',
+               'accuracy': 0.867,
+               'precision': 0.3363914373088685,
+               'recall': 0.6222889556222889,
+               'f1': 0.31562271508652207,
+               'type': 2}
+    return model01, model02
+
+
+def explain(model, text):
+    exp = LimeTextExplainer(class_names=["neutral", "bullying"]) \
+        .explain_instance(text, lambda x: predictions(model, x)[0], num_features=10)
+    return exp.as_list()
+
+
+def predictions(model, dataset):
+    lime_tokenizer = AutoTokenizer.from_pretrained("allegro/herbert-base-cased")
+    tokenized = [lime_tokenizer(x, truncation=True) for x in dataset]
+    trainer = Trainer(model=model, tokenizer=lime_tokenizer)
+    return trainer.predict(tokenized)
+
+
+def missed_predictions(model_path, dataset, num_labels):
+    model = AutoModelForSequenceClassification.from_pretrained(model_path, num_labels=num_labels)
+    pred = predictions(model, dataset['text'])[0]
+
+    pred_labels = np.argmax(pred, axis=1)
+    conf_matrix = confusion_matrix(dataset['label'], pred_labels)
+    disp = ConfusionMatrixDisplay(confusion_matrix=conf_matrix)
+    disp.plot()
+    plt.show()
+
+    zipped = list(zip(pred_labels, dataset['label']))
+    tp = zipped.index((1, 1))
+    tn = zipped.index((0, 0))
+    fp = zipped.index((1, 0))
+    fn = zipped.index((0, 1))
+
+    tp_explained = explain(model, dataset[tp]["text"])
+    tn_explained = explain(model, dataset[tn]["text"])
+    fp_explained = explain(model, dataset[fp]["text"])
+    fn_explained = explain(model, dataset[fn]["text"])
+
+    return [
+        {'tp': dataset[tp], 'idx': tp, 'expl': tp_explained},
+        {'tn': dataset[tn], 'idx': tn, 'expl': tn_explained},
+        {'fp': dataset[fp], 'idx': fp, 'expl': fp_explained},
+        {'fn': dataset[fn], 'idx': fn, 'expl': fn_explained},
+    ]
 
 
 if __name__ == '__main__':
@@ -85,14 +191,35 @@ if __name__ == '__main__':
 
     results = []
 
-    # bayesian_classifier(dataset1)
-    # fasttext_format('ds1', dataset1)
-    # fasttext_format('ds2', dataset2)
+    results.append(bayesian_classifier(dataset1, 1))
+    results.append(bayesian_classifier(dataset2, 2))
 
-    # results.append(fasttext_classifier('ds1', 'data_lab07'))
-    # results.append(fasttext_classifier('ds2', 'data_lab07'))
+    fasttext_format('ds1', dataset1)
+    fasttext_format('ds2', dataset2)
 
-    transformer_classifier(dataset1, "herbert-base-cased-bullying-1", 2)
-    transformer_classifier(dataset2, "herbert-base-cased-bullying-2", 3)
+    results.append(fasttext_classifier('ds1', 'data_lab07'))
+    results.append(fasttext_classifier('ds2', 'data_lab07'))
+
+    # id2label = {0: "non-harmful", 1: "harmful"}
+    # label2id = {"non-harmful": 0, "harmful": 1}
+
+    # transformer_classifier(dataset1, "roberta-detection-bullying-1", 2, id2label, label2id)
+    print("done training one")
+
+    id2label = {0: "non-harmful", 1: "cyberbullying", 2: "hate-speech"}
+    label2id = {"non-harmful": 0, "cyberbullying": 1, "hate-speech": 2}
+
+    # transformer_classifier(dataset2, "roberta-detection-bullying-2", 3, id2label, label2id)
+    print("done training two")
+
+    results.extend([*transform_results()])
+
+    df = pd.DataFrame(results)
+
+    with pd.option_context('display.max_rows', None, 'display.max_columns', None):
+        print(df[df['type'] == 1])
+        print(df[df['type'] == 2])
+
+    # res = missed_predictions('./models/mod_roberta-detection-bullying-1', dataset1['test'], 2)
 
     ...
---
+++ NLP/results/lab07/predictions.py
@@ -0,0 +1,38 @@
+# model01
+[
+    {'tp': {
+        'text': '@anonymized_account Jesteś 1 zjebem 4 rp', 'label': 1},
+        'idx': 187,
+        'expl': [
+            ('zjebem', 2.0851789352279235), ('anonymized_account', 0.663181463065831), ('Jesteś', 0.24438424546351667),
+            ('4', -0.10274716439163678), ('1', 0.035076622806751286), ('rp', -0.02532914432508221)]},
+    {'tn': {
+        'text': '@anonymized_account Spoko, jak im Duda z Morawieckim zamówią po pięć piw to wszystko będzie ok.',
+        'label': 0},
+        'idx': 0,
+        'expl': [
+            ('Morawieckim', 0.0646216084177749), ('Duda', 0.048453353295048206), ('będzie', -0.035555328198793555),
+            ('Spoko', 0.026873062030860928), ('piw', 0.0262589801609578), ('ok', -0.020868485146260652),
+            ('zamówią', -0.01808650639301773), ('pięć', -0.01784598624116778), ('to', -0.017225647435700375),
+            ('anonymized_account', -0.014822239487906428)]},
+    {'fp': {
+        'text': '@anonymized_account @anonymized_account @anonymized_account Prawda o Karczewskim to nie prawda o Polsce. Coś się Panu pojebało @anonymized_account .',
+        'label': 0},
+        'idx': 238,
+        'expl': [('pojebało', 1.9761960308872963),
+                 ('anonymized_account', 0.2253001829988589),
+                 ('Panu', 0.059809807120972634),
+                 ('Prawda', 0.05854528268888148),
+                 ('Coś', -0.045438095630831205),
+                 ('się', -0.02666762644469177),
+                 ('nie', 0.01244709383401292),
+                 ('Karczewskim', 0.011209533377531423),
+                 ('o', -0.008431621301889762),
+                 ('Polsce', -0.003794631051317719)]},
+    {'fn': {
+        'text': '@anonymized_account Tej szmaty się nie komentuje', 'label': 1},
+        'idx': 24,
+        'expl': [
+            ('szmaty', 1.8955300730459093), ('anonymized_account', 0.8423684372063593), ('Tej', 0.21789596592160954),
+            ('komentuje', -0.17214924133896436), ('nie', 0.06184502640055996), ('się', 0.024999475159384624)]}
+]
